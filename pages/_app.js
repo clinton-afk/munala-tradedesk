e3ed07be7e66af6016214d7cfee02b6e2240c1f7
clinton-afk <clinton@munalalogistics.co.ke>
2026-04-03 13:15:54 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,14 @@
+// pages/_app.js
+import '../styles/globals.css';
+import Header from '../components/Header';
 
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <main style={{ maxWidth: 1100, margin: '0 auto', padding: '0 18px 60px' }}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
